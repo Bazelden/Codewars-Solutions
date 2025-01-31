219fd738b93ff301576c2d045b5519b29888e683
Jackaroo <jack.waller@hotmail.com>
2025-01-31 20:14:06 +0000

    Added puzzle description

--- solutions.py
+++ solutions.py
@@ -1,5 +1,10 @@
 # https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/python # 8 Kyu
 
+# Complete the solution so that it reverses the string passed into it.
+
+# 'world'  =>  'dlrow'
+# 'word'   =>  'drow'
+
 def solution(string):
     rev_string = string[::-1]
     return rev_string
