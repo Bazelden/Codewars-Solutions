480366497401a68d85926d60ba40b63afdeb289d
Jackaroo <112336148+Bazelden@users.noreply.github.com>
2025-01-31 20:36:20 +0000

    Update count-by-x.py

--- 8-kyu/count-by-x/count-by-x.py
+++ 8-kyu/count-by-x/count-by-x.py
@@ -1,8 +1,10 @@
 def count_by(x, n):
-    #init an array with the first number as the first value
+    #init a list
     results = []
-        
-    for i in range(1, n + 1):
-        results.append(x * i)
     
+    # Loop through integers from 1 to n (inclusive)   
+    for i in range(1, n + 1):
+        # Multiply x by the current integer i and append the result to the list
+        results.append(x * i)
+    #return the list    
     return results  
