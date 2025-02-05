53abc1786a594c314c8c41b5522f31299f6953cd
Jackaroo <112336148+Bazelden@users.noreply.github.com>
2025-02-05 17:49:02 +0000

    Add solution - CodeHub

---
+++ 8-kyu/twice-as-old/twice-as-old.py
@@ -0,0 +1,9 @@
+def twice_as_old(dad_years_old, son_years_old):
+    #create variable to store the age of sons age, doubled.
+    father_twice_as_old = son_years_old * 2
+    #find a way to find the difference in years between dads current age and the target age set above
+    years_between_double_age = abs(dad_years_old - father_twice_as_old)
+    #return result
+    return years_between_double_age
+    
+    
