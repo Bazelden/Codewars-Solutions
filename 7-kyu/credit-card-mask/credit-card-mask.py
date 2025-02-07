fc821c2f9172156a4a5c150b77ffd604ff9b6036
Jackaroo <112336148+Bazelden@users.noreply.github.com>
2025-02-07 19:44:10 +0000

    Update credit-card-mask.py

--- 7-kyu/credit-card-mask/credit-card-mask.py
+++ 7-kyu/credit-card-mask/credit-card-mask.py
@@ -3,9 +3,11 @@ def maskify(cc):
    #split the string into a list?
     split_info = list(cc)
     print(split_info)
-    #iterate through the list and hash everything, except the final 4 items
+    #determine length of list and subtract 4 from the length - replace additional characters with #
     hashed_list = ['#'] * (len(split_info) - 4) + split_info[-4:]
+    #revert list back to string
     hashed_string = ''.join(hashed_list)
+    #return :) 
     return hashed_string
                 
 ​
