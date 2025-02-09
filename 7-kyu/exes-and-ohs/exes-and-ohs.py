50a0eaf95879a323ebfd96a4f868c9ad06bf31c8
Jackaroo <112336148+Bazelden@users.noreply.github.com>
2025-02-09 16:48:39 +0000

    Add solution - CodeHub

---
+++ 7-kyu/exes-and-ohs/exes-and-ohs.py
@@ -0,0 +1,18 @@
+def xo(s):
+    #create counters
+    x_amount = 0
+    o_amount = 0
+    
+    #iterate through the given string to check for an x or o and increase counter
+    for char in s.lower():  # Convert to lowercase for case-insensitivity
+        if char == 'x':
+            x_amount += 1
+        elif char == 'o':
+            o_amount += 1
+    
+    #check to see if amount of x and o are the same or non-existent and return true, else return false.
+    return x_amount == o_amount
+    
+    
+        
+    
